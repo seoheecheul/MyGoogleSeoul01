117f3fc64aa9f93b8acabc27487bca0a8e612db6
willby <beloglo79@gmail.com>
2015-07-16 21:02:53 +0900

    commit

---
+++ app/src/main/java/org/ex/mygoogleseoul01/GoogleCampusActivity.java
@@ -0,0 +1,20 @@
+package org.ex.mygoogleseoul01;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015-07-16.
+ */
+public class GoogleCampusActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_campus);
+
+    }
+
+}
